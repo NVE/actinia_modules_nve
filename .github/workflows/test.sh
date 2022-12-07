a5c5aac409e6f05178e9f92d514ca215c4ae976b
ninsbl <stbl@nve.no>
2022-12-07 09:22:02 +0000

    structure from grass addons

---
+++ .github/workflows/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# fail on non-zero return code from a subprocess
+set -e
+
+# Install module from addons if not available (in v7).
+if ! grass --tmp-location XY --exec g.download.location --help; then
+    grass --tmp-location XY --exec \
+        g.extension g.download.location
+fi
+grass --tmp-location XY --exec \
+    g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=$HOME
+
+grass --tmp-location XY --exec \
+    python3 -m grass.gunittest.main \
+        --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc \
+        --min-success 60
