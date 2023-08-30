d88ad3988117d4dd70af530b0296bfd8d00bb392
ninsbl <stefan.blumentrath@gmx.de>
2023-08-30 09:48:01 +0200

    add testcase

---
+++ src/general/g.unzip/testsuite/test_g_unzip.py
@@ -0,0 +1,62 @@
+"""Test i.satskred
+
+(C) 2023 by NVE, Stefan Blumentrath and the GRASS GIS Development Team
+This program is free software under the GNU General Public
+License (>=v2). Read the file COPYING that comes with GRASS
+for details.
+
+:authors: Stefan Blumentrath
+"""
+
+from pathlib import Path
+from zipfile import ZipFile
+
+import grass.script as gs
+
+from grass.gunittest.case import TestCase
+
+
+class TestGUnzipParallel(TestCase):
+    """Test case for parallel unzipping"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Initiate the working environment"""
+        cls.tempdir = Path(gs.tempdir())
+        for file_number in range(4):
+            with ZipFile(cls.tempdir / f"testfile_{file_number}.zip", "w") as test_zip:
+                test_zip.writestr(
+                    f"/testfile_{file_number}.txt", "This is test content!"
+                )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the temporary data"""
+        gs.utils.try_rmdir(cls.tempdir)
+
+    def test_g_unzip(self):
+        """Test unzipping to output directory while
+        removing zip files in parallel"""
+        # Check that zip-files are created
+        self.assertTrue(len(list(self.tempdir.glob("testfile_*.zip"))) == 4)
+        # Check that g.unzip runs successfully
+        self.assertModule(
+            "g.unzip",
+            input=str(self.tempdir),
+            output=str(self.tempdir / "output_directory"),
+            nprocs=2,
+            flags="r",
+            verbose=True,
+        )
+        # Check that zip-files are removed as requested
+        self.assertTrue(len(list(self.tempdir.glob("testfile_*.zip"))) == 0)
+        # Check that unzipped-files exist in the file system
+        self.assertTrue(
+            len(list((self.tempdir / "output_directory").glob("testfile_*.txt"))) == 4
+        )
+
+
+if __name__ == "__main__":
+    from grass.gunittest.main import test
+
+    test()
