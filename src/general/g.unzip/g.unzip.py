ca5329842f2fdaedeb5c9cdb65793c7fafeff0a3
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2023-08-31 09:54:43 +0200

    g.unzip: new helper-module to conveniently unpack Sentinel-archives in parallel (#14)
    * new module
    
    * new module
    
    * add testcase
    
    * add new module

---
+++ src/general/g.unzip/g.unzip.py
@@ -0,0 +1,111 @@
+#! /usr/bin/python3
+"""
+  MODULE:    g.unzip
+  AUTHOR(S): Stefan Blumentrath
+  PURPOSE:	 Unzip zip-files in a directory in parallel
+  COPYRIGHT: (C) 2023 by Stefan Blumentrath and the GRASS Development Team
+
+  This program is free software under the GNU General
+  Public License (>=v2). Read the file COPYING that
+  comes with GRASS for details.
+"""
+
+# %module
+# % description: Unzip zip-files in a directory in parallel
+# % keyword: general
+# % keyword: zip
+# % keyword: unzip
+# % keyword: unpack
+# %end
+
+# %option G_OPT_M_DIR
+# % key: input
+# % description: Path to the input directory with zip-files to extract
+# % required: yes
+# %end
+
+# %option G_OPT_M_DIR
+# % key: output
+# % description: Path to the output directory to extract zip-files to
+# % required: no
+# %end
+
+# %option G_OPT_M_NPROCS
+# %end
+
+# %flag
+# % key: r
+# % label: Remove zip-file(s) after extraction
+# % description: Remove zip-file(s) after extraction
+# %end
+
+import os
+import sys
+
+from functools import partial
+from multiprocessing import Pool
+from pathlib import Path
+from zipfile import ZipFile
+
+import grass.script as gs
+
+
+def unzip_file(file_path, out_dir=None, remove=False):
+    """Unzip file to output directory if given or current working directory
+    and remove unpacked zip-file if requested"""
+    gs.verbose(_("Unzipping {}").format(str(file_path)))
+    with ZipFile(file_path) as zip_file_object:
+        zip_file_object.extractall(path=out_dir)
+
+    if not remove:
+        return
+
+    file_path.unlink()
+    return
+
+
+def main():
+    """Do the main work"""
+    input_directory = Path(options["input"])
+    if not input_directory.exists():
+        gs.fatal(_("Input directory <{}> does not exist").format(str(input_directory)))
+    output_directory = input_directory
+    if options["output"]:
+        output_directory = Path(options["output"])
+    if not output_directory.exists():
+        try:
+            output_directory.mkdir(parents=True)
+        except OSError:
+            gs.fatal(
+                _("Cannot create output directory <{}>").format(str(output_directory))
+            )
+    if not os.access(output_directory, os.W_OK):
+        gs.fatal(
+            _("Output directory <{}> is not writeable").format(str(output_directory))
+        )
+
+    unzip = partial(unzip_file, out_dir=output_directory, remove=flags["r"])
+
+    input_files = list(input_directory.glob("*.[zZ][iI][pP]"))
+    if len(input_files) <= 0:
+        gs.warning(
+            _("No zip-files found in input directory <{}>").format(str(input_directory))
+        )
+
+    nprocs = min(int(options["nprocs"]), len(input_files))
+    gs.verbose(
+        _("Unzipping {} files to {} using {} parallel processes").format(
+            len(input_files), str(output_directory), nprocs
+        )
+    )
+    if nprocs == 1:
+        for zip_file in input_files:
+            unzip(zip_file)
+    else:
+        with Pool(nprocs) as pool:
+            pool.map(unzip, input_files)
+
+
+if __name__ == "__main__":
+    options, flags = gs.parser()
+    sys.exit(main())
