e97806b6c60fe90a70f23d22b2991e5df3ff9d7a
ninsbl <stefan.blumentrath@gmx.de>
2023-10-20 15:07:13 +0200

    rework grid reference

--- src/imagery/i.sentinel1.gdargeocode/i.sentinel1.gdargeocode.py
+++ src/imagery/i.sentinel1.gdargeocode/i.sentinel1.gdargeocode.py
@@ -29,7 +29,10 @@ COPYRIGHT:	(C) 2023 by NVE, Stefan Blumentrath
 # %option G_OPT_F_INPUT
 # %key: input
 # % description: Comma separated list of paths to Sentinel-1 SAFE files or single text file with list of Sentinel-1 SAFE files (one path per row)
-# % label: Path to GeoJSON file with the Area Of Interest (aoi)
+# % label: Path to input file(s)
+# %end
+
+# %option G_OPT_R_ELEV
 # %end
 
 # %option G_OPT_M_DIR
@@ -41,7 +44,6 @@ COPYRIGHT:	(C) 2023 by NVE, Stefan Blumentrath
 
 # %option G_OPT_F_INPUT
 # %key: aoi
-# % required: no
 # % description: Path to GeoJSON file with the Area Of Interest (aoi)
 # % label: Path to GeoJSON file with the Area Of Interest (aoi)
 # %end
@@ -51,15 +53,10 @@ COPYRIGHT:	(C) 2023 by NVE, Stefan Blumentrath
 # % type: string
 # % required: yes
 # % multiple: yes
-# % options: VV,VH
 # % description: Sentinel-1 polarization to geocode
-# % label: Currently only VV and VH are supported for Sentinel-1
+# % label: Currently only VV and VH from Sentinel-1 GRD IW are supported
+# % options: VV,VH
 # % answer: VH
-# %end
-
-# %flag
-# % key: a
-# % description: Apply precision orbit information if available
 # %end
 
 # %option
@@ -70,6 +67,38 @@ COPYRIGHT:	(C) 2023 by NVE, Stefan Blumentrath
 # % label: Currently only IW mode is supported for Sentinel-1
 # % options: IW
 # % answer: IW
+# %end
+
+# %option
+# % key: scale
+# % required: yes
+# % type: string
+# % description: Scaling applied to output values
+# % label: Currently only dbi scaling is supported
+# % multiple: no
+# % options: dbi
+# % answer: dbi
+# %end
+
+# %option
+# % key: suffix
+# % required: yes
+# % type: string
+# % description: Suffix added to geocoded file / map names
+# % answer: gec
+# %end
+
+# % option G_OPT_M_NPROCS
+# %end
+
+# %flag
+# % key: a
+# % description: Apply precision orbit information if available
+# %end
+
+# %flag
+# % key: t
+# % description: Write register file for registering results in TGIS
 # %end
 
 from datetime import datetime
@@ -83,41 +112,109 @@ import numpy as np
 import grass.script as gs
 
 
-def grass2rasterio(map_name):
-    """Open GRASS GIS raster map with rasterio"""
-    gdal_path = gs.find_file(map_name)["file"].replace("/cell/", "/cellhd/")
-    return rasterio.open(gdal_path)
+def get_raster_gdalpath(map_name, check_linked=True):
+    """Get get the path to a raster map that can be opened by GDAL
+    Checks for GDAL source of linked raster data and returns those
+    if not otherwise requested"""
+    if check_linked:
+        gis_env = gs.gisenv()
+        map_info = gs.find_file(map_name)
+        header_path = (
+            Path(gis_env["GISDBASE"])
+            / gis_env["LOCATION_NAME"]
+            / map_info["mapset"]
+            / "cell_misc"
+            / map_info["name"]
+            / "gdal"
+        )
+        if header_path.exists():
+            gdal_path = Path(
+                gs.parse_key_val(header_path.read_text().replace(": ", "="))["file"]
+            )
+            if gdal_path.exists():
+                return gdal_path
+        gdal_path = Path(
+            gs.parse_key_val(
+                gs.parse_command("r.info", flags="e", map=map_name)["comments"]
+                .replace("\\", "")
+                .replace('"', ""),
+                vsep=" ",
+            )["input"]
+        )
+        if gdal_path.exists():
+            return gdal_path
+    gdal_path = Path(gs.find_file(map_name)["file"].replace("/cell/", "/cellhd/"))
+    if gdal_path.exists():
+        return gdal_path
+    gs.fatal(_("Cannot determine GDAL readable path to raster map {}").format(map_name))
+
+
+def check_file_input(file_input):
+    """Checks input for files to geocode.
+    If input is a directory (and not SAFE), contained SAFE files are listed
+    If input is a text file each line is assumed to be a path to a SAFE file
+    If input is a comma separated list of files element is assumed to be a path to a SAFE file
+    Returns a sanetized list of Sentinel-1 input files.
+    """
+    file_input = options["input"].split(",")
+    if len(file_input) == 1:
+        file_input = Path(file_input[0])
+        if file_input.is_dir() and file_input.suffix != ".SAFE":
+            # Directory mode
+            file_input = list(file_input.glob("*.SAFE"))
+        elif file_input.suffix == ".SAFE":
+            # SAFE-file mode
+            file_input = check_files_list([str(file_input)])
+        elif file_input.suffix.lower() == ".zip":
+            gs.fatal(_("Geocoding of zipped files is currently not supported"))
+        else:
+            # Text file mode
+            file_input = file_input.read_text(encoding="UTF8").split("\n")
+            file_input = check_files_list(file_input)
+    else:
+        # File-list mode
+        file_input = check_files_list(file_input)
+    return file_input
+
+
+def check_files_list(file_path_list):
+    """Checks if files in a list of files exist and gives a warning otherwise"""
+    existing_paths = []
+    for file_path in file_path_list:
+        file_path_object = Path(file_path)
+        if file_path_object.exists():
+            if file_path_object.suffix.upper() == ".SAFE":
+                existing_paths.append(file_path_object)
+            else:
+                gs.warning(
+                    _(
+                        "Format of file {} not supported.\nOnly SAFE format is accepted."
+                    ).format(file_path)
+                )
+        gs.warning(_("File {} not found").format(file_path))
+    return existing_paths
 
 
 def grass2gdar(map_name):
-    """Build a single band GDAR raster object from a rasterio raster object"""
-    band = 1
-    rasterio_dataset = grass2rasterio(map_name)
-    return build_crsmeta(
-        rasterio_dataset.shape,
-        [
-            rasterio_dataset.bounds[1] + 0.5 * abs(rasterio_dataset.transform[4]),
-            rasterio_dataset.bounds[0] + 0.5 * abs(rasterio_dataset.transform[0]),
-        ],
-        [abs(rasterio_dataset.transform[4]), abs(rasterio_dataset.transform[0])],
-        (rasterio_dataset.crs).wkt,
-        dtype=np.dtype(rasterio_dataset.dtypes[band - 1]),
-        nodatavalue=rasterio_dataset.nodatavals[band - 1],
-        form=rasterio_dataset.meta["driver"],
-        desc=map_name,
-        aux=None,
-    )
+    """Read GRASS GIS raster map as a single band GDAR raster object"""
+    gdal_path = get_raster_gdalpath(map_name)
+    try:
+        return reader(str(gdal_path))
+    except ValueError:
+        gs.fatal(
+            _("Input digital elevation model {} is not a linked GeoTiff").format(
+                map_name
+            )
+        )
 
 
 def get_aoi_geometry(geojson_file):
     """Extract the Area of Interest AOI from a GeoJSON file and
     return it as an OGR Geometry object.
     The input GeoJSON should contain only one polygon geometry"""
-
     ogr_dataset = ogr.Open(
         f"/vsicurl/{geojson_file}" if geojson_file.startswith("http") else geojson_file
     )
-
     if not ogr_dataset:
         gs.fatal(_("Could not open AOI file <{}>").format(geojson_file))
     if ogr_dataset.GetLayerCount() > 1:
@@ -133,69 +230,82 @@ def get_aoi_geometry(geojson_file):
     return ogr_feature.geometry()
 
 
-def get_target_geometry(bpol, aoi=None, crs_wkt=None):
+def get_target_geometry(bpol, geojson_file=None, crs_wkt=None):
     """Intersect a bounding polygon represented aS an array of vertices
     or an ogr.Geometry with an area of interest if given and projects
     the relevant bounding geometry (target geometry) to the target CRS
     given in WKT format."""
-    aoi = get_aoi_geometry(aoi)
+    # aoi = get_aoi_geometry(aoi)
     s_srs = osr.SpatialReference()
     s_srs.ImportFromEPSG(4326)
     t_srs = osr.SpatialReference()
     t_srs.ImportFromWkt(crs_wkt)
     crs_transformer = osr.CoordinateTransformation(s_srs, t_srs)
-    if aoi:
+    if geojson_file:
+        ogr_dataset = ogr.Open(
+            f"/vsicurl/{geojson_file}"
+            if geojson_file.startswith("http")
+            else geojson_file
+        )
+        if not ogr_dataset:
+            gs.fatal(_("Could not open AOI file <{}>").format(geojson_file))
+        if ogr_dataset.GetLayerCount() > 1:
+            gs.warning(_("Input file contains more than one layer"))
+        ogr_layer = ogr_dataset.GetLayerByIndex(0)
+        if ogr_layer.GetGeomType() != 3:
+            gs.warning(_("GeoJSON does not contain polygons"))
+        if ogr_layer.GetFeatureCount() > 1:
+            gs.warning(
+                _("GeoJSON contains more than one geometry. Using only the first one.")
+            )
+        aoi = ogr_layer.GetFeature(0)
         if not isinstance(bpol, ogr.Geometry):
             coords = ", ".join([f"{point[1]} {point[0]}" for point in list(bpol)])
             bpol = ogr.CreateGeometryFromWkt(f"POLYGON(({coords}))")
-        if not bpol.Intersect(aoi):
+        if not bpol.Intersect(aoi.geometry()):
             return None
-        bpol = bpol.Intersection(aoi)
-
+        bpol = bpol.Intersection(aoi.geometry())
     if isinstance(bpol, ogr.Geometry):
         return np.array(
             [
-                [
+                crs_transformer.TransformPoint(
                     bpol.GetGeometryRef(0).GetY(vertex_id),
                     bpol.GetGeometryRef(0).GetX(vertex_id),
-                ]
+                )
                 for vertex_id in range(bpol.GetGeometryRef(0).GetPointCount())
             ]
-        )
-
+        )[:, 0:2]
     return np.array([crs_transformer.TransformPoint(*point) for point in bpol])[:, 0:2]
 
 
 def gdar_geocode(
     s1_file_path,
-    dem=None,
     use_precision_orbit=True,
-    aoi=None,
-    out_type="dbi",
-    mode="IW",
-    polarizations=("VH", "VV"),
-    suffix="gec",
+    module_options=None,
     output_directory=None,
 ):
     """Perform actual geocding"""
 
-    # Read Sentinel-1 file
-    try:
-        s1_file = reader(str(s1_file_path))
-    except Exception:
-        gs.warning(_("Cannot open file {}").format(str(s1_file_path)))
-        return None
+    mode = module_options["mode"]
+    suffix = module_options["suffix"]
+    polarizations = module_options["polarization"].split(",")
+    out_type = module_options["scale"]
 
+    # Read Sentinel-1 file
+    s1_file = reader(str(s1_file_path))
     if use_precision_orbit:
         gs.debug(_("Accessing precision orbits from remote..."))
         try:
             s1_file = sentinel1_orbit.update_trajectory_from_remote(s1_file)
-        except ConnectionError as e:
+        except ConnectionError as connection_error:
             gs.warning(
                 _(
-                    "Failed to update trajectory of {file_path} from ESA API. \n{error}\nProceeding anyways."
-                ).format(file_path=s1_file_path, error=e)
+                    "Failed to update trajectory of {file_path} from ESA API. \n{error}\nProceeding anyway."
+                ).format(file_path=s1_file_path, error=connection_error)
             )
+
+    # Read DEM into GDAR raster (resulting object cannot be pickled)
+    dem = grass2gdar(module_options["elevation"])[0]
 
     # Gety sensing time
     sensing_time = datetime.fromisoformat(
@@ -204,6 +314,9 @@ def gdar_geocode(
         )
     )
 
+    # Get track informarion
+    track = s1_file[mode][polarizations[0]].meta.track
+
     # Get track direction
     direction = (
         "descending"
@@ -211,66 +324,64 @@ def gdar_geocode(
         else "ascending"
     )
 
-    # Get track info
-    # track = s1_file[mode][polarization].meta.track
-
     # Get footprint within AOI in current location CRS
     bounding_polygon = get_target_geometry(
-        s1_file.get_boundingpolygon(), aoi=aoi, crs_wkt=dem.grid.refsys.wkt
+        s1_file.get_boundingpolygon(),
+        geojson_file=module_options["aoi"],
+        crs_wkt=dem.meta.grid.refsys.wkt,
     )
-    # Set region aligned to dem
+
+    # Set region aligned to DEM
     region = gs.parse_command(
         "g.region",
         flags="ug",
-        align=dem.description,
+        align=Path(module_options["elevation"]).stem,
         n=max(bounding_polygon[:, 1]),
         s=min(bounding_polygon[:, 1]),
         e=max(bounding_polygon[:, 0]),
         w=min(bounding_polygon[:, 0]),
     )
+
     # Create target grid
+    # from gdar.gridtools import grid_from_bpol
+    # https://github.com/NVE/satskred_dev/blob/5ff9e47f41da2796d91e9af0744e4cf34caa5c58/satskred/processors/woodpecker/util.py#L292
     grid = build_crsgrid(
         {
-            "origin": [region["n"], region["w"]],
-            "shape": [int(region["rows"]), int(region["cols"])],
+            "origin": [
+                float(region["s"]) - (float(region["nsres"]) / 2.0),
+                float(region["w"]) - float(region["ewres"]) / 2.0,
+            ],
+            "shape": [int(region["rows"]) + 1, int(region["cols"]) + 1],
             "offset": [0, 0],
             "samplespacing": [float(region["nsres"]), float(region["ewres"])],
-            "wkt": dem.grid.refsys.wkt,
+            "wkt": dem.meta.grid.refsys.wkt,
         }
     )
 
-    # Resampling (currently not implemented)
-    # if :
-    #    s1_file = rastertools.downsample(s1_file, (3, 3), average=True)
-    # elif :
-    #     s1_file = rastertools.upsample(s1_file, (3, 3), average=True)
+    # Resampling (up or down) dependent on DEM resolution (currently not implemented)
+    # https://sentinels.copernicus.eu/web/sentinel/user-guides/Sentinel-1-sar/resolutions/level-1-ground-range-detected
+    # if float(region["nsres"]) < 20.0 or float(region["ewres"]) < 20.0:
+    s1_file = rastertools.downsample(s1_file, (3, 3), average=True)
+    # else:
+    #    s1_file = rastertools.upsample(s1_file, (3, 3), average=True)
 
     # Geocode
     gec = geocoding(s1_file, dem, order=3, out_type=out_type, grid=grid)
 
+    #
     register_strings = []
     for polarization in polarizations:
-        map_name = f"{s1_file_path.stem}_{polarization}_{suffix}"
+        map_name = f"{s1_file_path.stem}_{suffix}_{polarization}_{out_type}_{track}_{direction}"
         output_file = output_directory / f"{map_name}.tif"
         # Write file
-        write_crs(gec[mode][polarization], output_file)
+        write_crs(gec[mode][polarization], str(output_file))
         # Link resulting GeoTiff
         gs.run_command("r.external", flags="om", input=output_file, output=map_name)
-        register_strings.append(f"{map_name}|{sensing_time}|s1_intensity_{direction}")
+        register_strings.append(
+            f"{map_name}|{sensing_time}|s1_{polarization}_backscatter_{out_type}_{track}_{direction}"
+        )
 
     return "\n".join(register_strings)
-
-
-def check_files_list(file_path_list):
-    """Checks files in a list of files exist and gives a warning otherwise"""
-    existing_paths = []
-    for file_path in file_path_list:
-        file_path_object = Path(file_path)
-        if file_path_object.exists():
-            existing_paths.append(file_path_object)
-        gs.warning(_("File {} not found").format(file_path))
-
-    return existing_paths
 
 
 def main():
@@ -287,39 +398,19 @@ def main():
             )
         )
 
-    # S1 files
-    # identify files to geocode
-    file_input = options["input"].split(",")
-    if len(file_input) == 1:
-        file_input = Path(file_input[0])
-        if file_input.is_dir():
-            # Directory mode
-            file_input = list(file_input.glob("*.SAFE"))
-        elif file_input.suffix == "SAFE":
-            file_input = check_files_list([file_input])
-        else:
-            # File mode
-            file_input = file_input.read_text(encoding="UTF8").split("\n")
-            file_input = check_files_list(file_input)
-    else:
-        # File-list mode
-        file_input = check_files_list(file_input)
+    # identify Sentinel-1 SAFE files to geocode
+    file_input = check_file_input(options["input"])
 
+    # Check if files to geocode are found
     if len(file_input) < 1:
         gs.fatal(_("No Sentinel-1 files found to Geocode"))
-
-    # Read DEM into GDAR raster
-    dem = grass2gdar(options["elevation"])
 
     # Geocode files (in parallel if requested)
     # Setup function
     _geocode = partial(
         gdar_geocode,
-        dem=dem,
         use_precision_orbit=flags["a"],
-        suffix="gec",
-        mode=options["mode"],
-        polarization=options["polarization"],
+        module_options=options,
         output_directory=output_directory,
     )
     nprocs = int(options["nprocs"])
@@ -331,20 +422,22 @@ def main():
         for s1_file in file_input:
             geocoded_files.append(_geocode(s1_file))
 
-    # Write registration files
-    Path(options["register_file"]).write_text(
-        "\n".join(geocoded_files), encoding="UTF8"
-    )
+    if flags["t"]:
+        # Write registration files
+        Path(options["register_file"]).write_text(
+            "\n".join(geocoded_files), encoding="UTF8"
+        )
+    else:
+        print("\n".join(geocoded_files))
 
 
 if __name__ == "__main__":
     options, flags = gs.parser()
 
     try:
-        # from gdar import rastertools
+        from gdar import rastertools
         from gdar.fileformats import write_crs
         from gdar.gridtools import is_descending, build_crsgrid
-        from gdar.metatools import build_crsmeta
         from gdar.readers import reader, sentinel1_orbit
         from gdargeocoding.geocode import geocoding
     except ImportError:
@@ -361,16 +454,6 @@ if __name__ == "__main__":
         gs.fatal(
             _(
                 "Can not import GDAL python bindings. Please install it with 'pip install GDAL==${GDAL_VERSION}'"
-            )
-        )
-
-    try:
-        import rasterio
-
-    except ImportError:
-        gs.fatal(
-            _(
-                "Can not import rasterio library. Please install it with 'pip install rasterio'"
             )
         )
 
