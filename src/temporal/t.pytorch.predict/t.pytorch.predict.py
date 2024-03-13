8b293323fa466222c75c8055e8855ad6d917158d
ninsbl <stefan.blumentrath@gmx.de>
2024-03-13 10:56:35 +0100

    unix LF, propagate new flag and option

--- src/temporal/t.pytorch.predict/t.pytorch.predict.py
+++ src/temporal/t.pytorch.predict/t.pytorch.predict.py
@@ -70,7 +70,7 @@
 # % type: integer
 # % required: no
 # % multiple: yes
-# % description: Number of cows and columns in tiles (rows, columns)
+# % description: Number of rows and columns in tiles (rows, columns)
 # %end
 
 # %option
@@ -78,12 +78,19 @@
 # % type: integer
 # % required: no
 # % multiple: no
-# % description: Number of cows and columns of overlap in tiles
+# % description: Number of rows and columns of overlap in tiles
 # %end
 
 # %option G_OPT_F_INPUT
 # % key: configuration
 # % description: Path to JSON file with band configuration in the input deep learning model
+# %end
+
+# %option G_OPT_F_INPUT
+# %key: mask_json
+# % required: no
+# % multiple: no
+# % description: JSON file with one or more mask band or map name(s) and reclass rules for masking, e.g. {"mask_band": "1 thru 12 36 = 1", "mask_map": "0"}
 # %end
 
 # %option
@@ -117,16 +124,22 @@
 # % description: Use CPU as device for prediction, default is use cuda (GPU) if detected
 # %end
 
+# %flag
+# %key: l
+# % description: Limit output to valid range (data outside the valid range is set to valid min/max)
+# %end
+
 # %rules
 # % collective: title,description
 # % required: -e,title,description
 # %end
 
 import os
-import re
+
+# import re
 import sys
 
-from datetime import datetime
+# from datetime import datetime
 from functools import partial
 from math import floor
 from multiprocessing import Pool
@@ -139,6 +152,7 @@ import grass.script as gs
 TMP_NAME = gs.tempname(12)
 # Get GRASS GIS environment
 GISENV = dict(gs.gisenv())
+OVERWRITE = gs.overwrite()
 
 
 def distribute_cores(nprocs, groups_n):
@@ -163,9 +177,10 @@ def process_scene_group(
     basename=None,
     module_options=None,
     semantic_label=None,
-    cpu=True,
+    torch_flags=None,
 ):
-    """Import a group of Sentinel3 scenes"""
+    """Create an imagery group from semantic labels of a temporal extent and
+    run a pytorch prediction on the imagery group"""
     if not basename:
         output_name = f"{os.path.commonprefix(map_list)}{semantic_label}"
     else:
@@ -178,7 +193,7 @@ def process_scene_group(
         output=output_name,
         stdout_=PIPE,
         **module_options,
-        flags="c" if cpu else None,
+        flags=torch_flags,
         quiet=True,
     )
     return f"{output_name}@{GISENV['MAPSET']}|{temporal_extent[0].isoformat()}|{temporal_extent[1].isoformat()}|{semantic_label}"
@@ -192,12 +207,13 @@ def main():
 
     # Initialize SpaceTimeRasterDataset (STRDS) using tgis
     strds_long_name = f"{options['output']}@{GISENV['MAPSET']}"
-    tgis_strds = tgis.SpaceTimeRasterDataset(strds_long_name)
+    output_strds = tgis.SpaceTimeRasterDataset(strds_long_name)
+    output_strds_in_db = output_strds.is_in_db()
 
     # Check if input is complete and valid
     # Check if target STRDS exists and create it if not or abort if overwriting is not allowed
-    if tgis_strds.is_in_db():
-        if not gs.overwrite():
+    if output_strds_in_db:
+        if not OVERWRITE:
             gs.fatal(
                 _(
                     "Output STRDS <{}> exists."
@@ -212,7 +228,7 @@ def main():
         )
 
     # Get list of maps in input STRDS
-    input_strds = tgis.open_old_stds(options["output"], "strds", dbif)
+    input_strds = tgis.open_old_stds(options["input"], "strds", dbif)
     map_rows = input_strds.get_registered_maps(
         ",".join(["id", "start_time", "end_time", "semantic_label"]),
         options["where"],
@@ -223,7 +239,7 @@ def main():
     # Group maps using granule
     map_groups = {}
     for row in map_rows:
-        if not (row["start_time"], row["end_time"]) in map_groups:
+        if (row["start_time"], row["end_time"]) not in map_groups:
             map_groups[(row["start_time"], row["end_time"])] = {
                 row["semantic_label"]: row["id"]
             }
@@ -234,25 +250,36 @@ def main():
 
     # Distribute cores
     nprocs_inner, nprocs_outer = distribute_cores(
-        int(options["nprocs"]), len(groups_to_process)
+        int(options["nprocs"]), len(map_groups)
     )
 
     # Collect basic module_options for i.pytorch.predict
-    # Setup import module, and collect flags amd options
     module_options = {
         option: options[option]
-        for option in ["model", "model_code", "tile_size", "overlap", "configuration"]
+        for option in [
+            "model",
+            "model_code",
+            "tile_size",
+            "overlap",
+            "configuration",
+            "mask_json",
+        ]
     }
     module_options["nprocs"] = nprocs_inner
 
+    # Collect pytorch flags
+    torch_flags = [flag for flag in "lc" if flags[flag]]
+
+    # Setup prediction module function
     i_pytorch_predict = partial(
         process_scene_group,
         module_options=module_options,
         basename=options["basename"],
         semantic_label=options["semantic_label"],
-        cpu=True,
+        torch_flags=torch_flags,
     )
 
+    # Run predictions and collect
     if nprocs_outer > 1:
         with Pool(nprocs_outer) as pool:
             register_strings = pool.map(i_pytorch_predict, map_groups.items())
@@ -261,33 +288,8 @@ def main():
             i_pytorch_predict(scene_group) for scene_group in map_groups.items()
         ]
 
-    # Run i.pytorch predict
-    if nprocs > 1:
-        register_strings = pool.map(i_pytorch_predict, map_groups.items())
-    else:
-        register_strings = [
-            i_pytorch_predict(group, map_list) for group, map_list in map_groups.items()
-        ]
-
-    # Collect metadata and temporal register information
-
-    # Register results in output STRDS
-
-    # Initialize SpaceTimeRasterDataset (STRDS) using tgis
-    strds_long_name = f"{options['output']}@{GISENV['MAPSET']}"
-    tgis_strds = tgis.SpaceTimeRasterDataset(strds_long_name)
-
-    # Check if target STRDS exists and create it if not or abort if overwriting is not allowed
-    if tgis_strds.is_in_db() and not gs.overwrite():
-        gs.fatal(
-            _(
-                "Output STRDS <{}> exists."
-                "Use --overwrite together with -e to modify the existing STRDS."
-            ).format(options["output"])
-        )
-
     # Create STRDS if needed
-    if not tgis_strds.is_in_db() or (gs.overwrite() and not flags["e"]):
+    if not output_strds_in_db or (OVERWRITE and not flags["e"]):
         tgis.open_new_stds(
             options["output"],
             "strds",
@@ -296,14 +298,14 @@ def main():
             options["description"],
             "mean",
             None,
-            gs.overwrite(),
+            OVERWRITE,
         )
 
     # Write registration file with unique lines
     tmp_file = gs.tempfile(create=False)
     Path(tmp_file).write_text("\n".join(register_strings) + "\n", encoding="UTF8")
 
-    # Register downloaded maps in STRDS
+    # Register results in output STRDS
     register_maps_in_space_time_dataset(
         "raster",
         strds_long_name,
