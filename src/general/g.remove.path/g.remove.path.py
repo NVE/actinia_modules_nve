46dcfae19b14475601312903df7637fa24c41121
ninsbl <stefan.blumentrath@gmx.de>
2024-09-27 15:01:37 +0200

    new module to cleanup tempfiles

---
+++ src/general/g.remove.path/g.remove.path.py
@@ -0,0 +1,88 @@
+#! /usr/bin/python3
+"""
+MODULE:    g.remove.path
+AUTHOR(S): Stefan Blumentrath
+PURPOSE:   Remove temporary files or directories
+COPYRIGHT: (C) 2024 by Stefan Blumentrath and the GRASS Development Team
+
+This program is free software under the GNU General
+Public License (>=v2). Read the file COPYING that
+comes with GRASS for details.
+"""
+
+# %module
+# % description: Remove temporary files or directories
+# % keyword: general
+# % keyword: remove
+# % keyword: file
+# % keyword: directory
+# % keyword: cleanup
+# %end
+
+# %option
+# % key: paths
+# % description: Path to the file or directory to remove
+# % required: yes
+# %end
+
+# %flag
+# % key: f
+# % label: Force removal
+# % description: Force removal of files or directories
+# %end
+
+# %flag
+# % key: r
+# % label: Remove directories recursively
+# % description: Remove directories recursively
+# %end
+
+# ruff: noqa: PTH207
+
+import shutil
+import sys
+from glob import glob
+from pathlib import Path
+
+import grass.script as gs
+
+
+def main():
+    """Do the main work"""
+    options, flags = gs.parser()
+    paths_to_remove = glob(options["paths"])
+    if not paths_to_remove:
+        gs.warning(_("Nothing found to remove with <{}>.").format(options["paths"]))
+
+    if flags["f"]:
+        gs.info(_("Removing the following files and directories:"))
+        for user_path in paths_to_remove:
+            user_path = Path(user_path)
+            if user_path.is_symlink() or user_path.is_file():
+                try:
+                    user_path.unlink()
+                except Exception:
+                    gs.warning(
+                        _("Could not remove file or symlink <{}>").format(user_path)
+                    )
+            elif user_path.is_dir():
+                if flags["r"]:
+                    try:
+                        shutil.rmtree(str(user_path))
+                    except Exception:
+                        gs.warning(
+                            _("Could not remove directory <{}>").format(user_path)
+                        )
+                gs.warning(
+                    _(
+                        "Cannot remove <{}>. It is a directory. Use the r-flag to remove it."
+                    ).format(user_path)
+                )
+    else:
+        gs.info(_("Set to remove the following files and directories:"))
+        gs.info(_("Use the f-flag to actually remove them."))
+        gs.info(_("\n".join(paths_to_remove)))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/general/g.remove.path/testsuite/test_g_remove_path.py
@@ -0,0 +1,117 @@
+"""Test i.satskred
+
+(C) 2023 by NVE, Stefan Blumentrath and the GRASS GIS Development Team
+This program is free software under the GNU General Public
+License (>=v2). Read the file COPYING that comes with GRASS
+for details.
+
+:authors: Stefan Blumentrath
+"""
+
+from pathlib import Path
+
+import grass.script as gs
+from grass.gunittest.case import TestCase
+from grass.gunittest.gmodules import SimpleModule
+
+
+class TestGUnzipParallel(TestCase):
+    """Test case for removing files and directories"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Initiate the working environment"""
+        cls.tempdir = Path(gs.tempdir())
+        (cls.tempdir / "dir_1").mkdir(parents=True, exist_ok=True)
+        (cls.tempdir / "dir_2").mkdir(parents=True, exist_ok=True)
+        (cls.tempdir / "dir_3").mkdir(parents=True, exist_ok=True)
+        (cls.tempdir / "dir_4").mkdir(parents=True, exist_ok=True)
+        (cls.tempdir / "dir_4" / "dir_1").mkdir(parents=True, exist_ok=True)
+        (cls.tempdir / "file_1").write_text("", encoding="UTF8")
+        (cls.tempdir / "file_2").write_text("", encoding="UTF8")
+        (cls.tempdir / "file_3").write_text("", encoding="UTF8")
+        (cls.tempdir / "dir_3" / "file_1").write_text("", encoding="UTF8")
+        (cls.tempdir / "dir_4" / "file_1").write_text("", encoding="UTF8")
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the temporary data"""
+        gs.utils.try_rmdir(cls.tempdir)
+
+    def test_g_remove_path_wildcard_no_removal(self):
+        """Test file removal dry-run with wildcard"""
+        tmp_files_before = list(self.tempdir.glob("*"))
+        tmp_files_before.sort()
+        # Check that g.remove.path runs successfully
+        g_remove_list = SimpleModule(
+            "g.remove.path",
+            paths=f"{self.tempdir}/*",
+        ).run()
+
+        tmp_files_after = list(self.tempdir.glob("*"))
+        tmp_files_after.sort()
+        # Check that no files are removed
+        self.assertTrue(
+            [str(tmp_file) for tmp_file in tmp_files_before]
+            == [str(tmp_file) for tmp_file in tmp_files_after]
+        )
+        self.assertTrue(
+            all(
+                str(tmp_file) in g_remove_list.outputs.stderr
+                for tmp_file in tmp_files_before
+            )
+        )
+
+    def test_g_remove_path_wildcard_no_dir_removal(self):
+        """Test file removal dry-run with wildcard"""
+        tmp_files_before = list(self.tempdir.glob("dir*"))
+        tmp_files_before.sort()
+        # Check that g.remove.path runs successfully
+        g_remove_list = SimpleModule(
+            "g.remove.path", paths=f"{self.tempdir}/dir*", flags="f"
+        ).run()
+
+        tmp_files_after = list(self.tempdir.glob("dir*"))
+        tmp_files_after.sort()
+        # Check that no files are removed
+        self.assertTrue(
+            [str(tmp_file) for tmp_file in tmp_files_before]
+            == [str(tmp_file) for tmp_file in tmp_files_after]
+        )
+        self.assertTrue(
+            all(
+                str(tmp_file) in g_remove_list.outputs.stderr
+                for tmp_file in tmp_files_before
+            )
+        )
+
+    def test_g_remove_path_wildcard_with_removal(self):
+        """Test file removal with wildcard"""
+        # Check that g.remove.path runs successfully
+        g_remove_list = SimpleModule(
+            "g.remove.path",
+            paths=f"{self.tempdir}/dir_3/*",
+            flags="rf",
+        ).run()
+
+        # Check that no files are removed
+        self.assertTrue(len(list((self.tempdir / "dir_3").glob("*"))) == 0)
+
+    def test_g_remove_path_no_dir_removal(self):
+        """Test file removal with wildcard"""
+        # Check that g.remove.path runs successfully
+        g_remove_list = SimpleModule(
+            "g.remove.path",
+            paths=f"{self.tempdir}/dir_4/*",
+            flags="f",
+        ).run()
+
+        self.assertTrue("WARNING" in g_remove_list.outputs.stderr)
+        # Check that no dirs are removed
+        self.assertTrue(len(list((self.tempdir / "dir_4").glob("*"))) == 1)
+
+
+if __name__ == "__main__":
+    from grass.gunittest.main import test
+
+    test()
