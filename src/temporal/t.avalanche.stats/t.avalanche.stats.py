a7541b21ddaae2a0ca23263e65d3de0f1dde5938
ninsbl <stefan.blumentrath@gmx.de>
2026-03-02 10:56:08 +0100

    change also column names from dtm to dem

--- src/temporal/t.avalanche.stats/t.avalanche.stats.py
+++ src/temporal/t.avalanche.stats/t.avalanche.stats.py
@@ -63,7 +63,6 @@ COPYRIGHT: (C) 2024 by NVE, Stefan Blumentrath
 
 import atexit
 import sys
-import os
 from pathlib import Path
 from subprocess import PIPE
 from zipfile import ZipFile
@@ -102,7 +101,7 @@ def zip_shape(shape_file):
     # Write to zip-file
     with ZipFile(str(directory / f"{base_name}.zip"), "w") as zf:
         for shape_file_part in shape_file_parts:
-            zf.write(shape_file_part, os.path.basename(shape_file_part))
+            zf.write(shape_file_part, shape_file_part.name)
             shape_file_part.unlink()
 
 
@@ -158,8 +157,8 @@ def process_avalanche_map(avalanche_map_row, **kwargs):
             "pol TEXT,"
             "sat_geom INTEGER,"
             "direction TEXT, algoritme TEXT,"
-            "dtm_min REAL,dtm_mean REAL,"
-            "dtm_max REAL,slp_min REAL,"
+            "dem_min REAL,dem_mean REAL,"
+            "dem_max REAL,slp_min REAL,"
             "slp_mean REAL, slp_max REAL,"
             "asp_min REAL, asp_mean REAL,"
             "asp_max REAL"
