c64e5030835c037f66ac84b3dbc1990ba6005e54
ninsbl <stefan.blumentrath@gmx.de>
2026-09-24 09:07:37 +0200

    prevent blocking of multiprocessing

--- src/general/g.transfer/g.transfer.py
+++ src/general/g.transfer/g.transfer.py
@@ -66,7 +66,12 @@ def transfer(source, target=None, move=False, overwrite=False):
         target_name = target / source.name
         if target_name.exists():
             if not overwrite:
-                gs.fatal(_("Target <{}> exists. Please use the overwrite flag."))
+                # Raise instead of gs.fatal() to not block multiprocessing
+                raise RuntimeError(
+                    _("Target <{}> exists. Please use the overwrite flag.").format(
+                        target_name
+                    )
+                )
             target_name.unlink()
         if move:
             gs.verbose(
@@ -86,7 +91,7 @@ def transfer(source, target=None, move=False, overwrite=False):
         )
         target_dir = target / source.name
         if target_dir.exists() and not overwrite:
-            gs.fatal(
+            raise RuntimeError(
                 _("Target <{}> exists. Please use the overwrite flag.").format(
                     str(target_dir)
                 )
@@ -123,12 +128,15 @@ def main():
     )
     nprocs = int(options["nprocs"]) or os.cpu_count() or 1
 
-    if nprocs > 1:
-        with Pool(nprocs) as pool:
-            pool.map(transfer_function, paths_to_transfer)
-    else:
-        for transfer_path in paths_to_transfer:
-            transfer_function(transfer_path)
+    try:
+        if nprocs > 1:
+            with Pool(nprocs) as pool:
+                pool.map(transfer_function, paths_to_transfer)
+        else:
+            for transfer_path in paths_to_transfer:
+                transfer_function(transfer_path)
+    except RuntimeError as error:
+        gs.fatal(str(error))
 
 
 if __name__ == "__main__":
